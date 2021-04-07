b76975cfb054f241537dae160a0c0f0147c869c4
Jared Hughes <jahughes241@gmail.com>
2021-04-06 18:33:46 -0700

    Migrate to Manifest v3
    More future-proof

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -2,7 +2,8 @@ const { merge } = require('webpack-merge')
 const webpackConfig = require('./webpack.config.base')
 
 const cfg = merge(webpackConfig, {
-  devtool: 'eval-source-map',
+  // can't use eval- in Manifest v3 extension
+  devtool: 'inline-cheap-module-source-map',
   watch: true,
   watchOptions: {
     ignored: /node_modules/
