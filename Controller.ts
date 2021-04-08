28ce37e8b7cb873d76e9d1e704e0a7509868eda7
Jared Hughes <jahughes241@gmail.com>
2021-04-08 02:23:25 -0700

    Add export as mp4 via FFmpeg
    Huge thanks to @SlimRunner with the FFmpeg.wasm reference: 
    https://gist.github.com/SlimRunner/3b0a7571f04d3a03bff6dbd9de6ad729

--- Controller.ts
+++ Controller.ts
@@ -1,6 +1,10 @@
 import View from './View'
 import { Calc } from 'desmodder'
 
+// kinda jank, but switching to moduleResolution: 'node' messes up
+// existing non-relative imports
+import { createFFmpeg, fetchFile } from './node_modules/@ffmpeg/ffmpeg/src/index.js'
+
 type PNGDataURI = string
 
 export default class Controller {
@@ -8,6 +12,7 @@ export default class Controller {
   frames: PNGDataURI[] = []
   isMainViewOpen: boolean = false
   isCapturing: boolean = false
+  isExporting: boolean = false
 
   init (view: View) {
     this.view = view
@@ -49,5 +54,53 @@ export default class Controller {
     await this._captureFrame()
     this.isCapturing = false
     this.updateView()
+  }
+
+  async exportFrames () {
+    // reference https://gist.github.com/SlimRunner/3b0a7571f04d3a03bff6dbd9de6ad729#file-desmovie-user-js-L278
+    const ffmpeg = createFFmpeg({ log: true });
+    await ffmpeg.load()
+
+    const filenames: string[] = []
+
+    this.frames.forEach(async (frame, i) => {
+      const filename = `desmos.${i}.png`
+      // filenames may be pushed out of order because async, but doesn't matter
+      filenames.push(filename)
+      ffmpeg.FS('writeFile', filename, await fetchFile(frame))
+    })
+
+    this.isExporting = true
+    this.updateView()
+
+    const outFilename = 'out.mp4'
+
+    await ffmpeg.run('-r', '60', '-pattern_type', 'glob', '-i', '*.png', '-c:v', 'libx264', '-pix_fmt', 'yuv420p', outFilename);
+    const data = ffmpeg.FS('readFile', outFilename)
+    filenames.forEach(filename => {
+      ffmpeg.FS('unlink', filename)
+    })
+    ffmpeg.FS('unlink', outFilename)
+    const url = URL.createObjectURL(new Blob([data.buffer as ArrayBuffer], { type: 'video/mp4' }))
+
+    const humanOutFilename = 'DesModder\ Video\ Creator.mp4'
+    this.download(url, humanOutFilename)
+
+    this.isExporting = false
+    this.updateView()
+  }
+
+  download(url: string, filename: string) {
+    // https://gist.github.com/SlimRunner/3b0a7571f04d3a03bff6dbd9de6ad729#file-desmovie-user-js-L325
+    // no point supporting anything besides Chrome (no SharedArrayBuffer support)
+    var a = document.createElement("a");
+    a.href = url;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+    setTimeout(function() {
+      document.body.removeChild(a);
+      window.URL.revokeObjectURL(url);
+    }, 0);
   }
 }
--- components/MainPopup.tsx
+++ components/MainPopup.tsx
@@ -19,11 +19,23 @@ export default class MainPopup extends DCGView.Class {
           role='button'
           class={() => ({
             'gif-creator-capture-frame-button': true,
-            'dcg-btn-green': !this.controller.isCapturing
+            'dcg-btn-green': !this.controller.isCapturing && !this.controller.isExporting
           })}
           onTap={() => this.controller.captureOneFrame()}
         >
           One frame
+        </span>
+        {/* TODO: segmented select with file type gif/mp4/webm */}
+        {/* TODO: fps option */}
+        <span
+          role='button'
+          class={() => ({
+            'gif-creator-export-frames-button': true,
+            'dcg-btn-green': this.controller.frames.length > 0 && !this.controller.isCapturing && !this.controller.isExporting
+          })}
+          onTap={() => this.controller.exportFrames()}
+        >
+          Export as mp4
         </span>
       </div>
     )
