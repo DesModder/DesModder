fea7225116652d6e49d793d02a9597d118723143
Jared Hughes <jahughes241@gmail.com>
2021-04-17 00:32:22 -0700

    Allow disabling a SegmentedControl

--- src/components/SegmentedControl.tsx
+++ src/components/SegmentedControl.tsx
@@ -1,11 +1,13 @@
 import DCGView from 'DCGView'
 import { mergeClass, MaybeClassDict } from 'utils'
+import './SegmentedControl.less'
 
 export default class SegmentedControl extends DCGView.Class<{
   names: string[],
   selectedIndex: number,
   setSelectedIndex (i: number): void,
   class?: MaybeClassDict
+  allowChange?: boolean
 }> {
   template () {
     return (
@@ -20,17 +22,27 @@ export default class SegmentedControl extends DCGView.Class<{
               class={() => ({
                 'dcg-segmented-control-btn': true,
                 'dcg-dark-gray-segmented-control-btn': true,
-                'dcg-selected dcg-active': i === this.props.selectedIndex()
+                'dcg-selected dcg-active': i === this.props.selectedIndex(),
+                'desmodder-disallow-change': !this.getChangeAllowed(i)
               })}
               role='button'
               ariaLabel={name}
-              onTap={() => this.props.setSelectedIndex(i)}
+              onTap={() => this.getChangeAllowed(i) && this.props.setSelectedIndex(i)}
             >
               {name}
             </div>
           ))
         }
       </div>
+    )
+  }
+
+  getChangeAllowed(i: number) {
+    const allowChange = this.props.allowChange
+    return (
+      allowChange === undefined ||
+      allowChange() ||
+      i === this.props.selectedIndex()
     )
   }
 }
