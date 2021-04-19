a16aba86048eeabf06e60b25b92c7b9266319bcf
Jared Hughes <jahughes241@gmail.com>
2021-04-19 00:31:08 -0700

    Initial commit

---
+++ index.ts
@@ -0,0 +1,15 @@
+function onEnable() {
+  // template
+}
+
+function onDisable() {
+  // template
+}
+
+export default {
+  name: "Template plugin name",
+  description: "Template plugin description",
+  onEnable: onEnable,
+  onDisable: onDisable,
+  enabledByDefault: true,
+};
