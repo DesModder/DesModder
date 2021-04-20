39d3254612a926bf1961736ab1be0206f27c0c3f
Jared Hughes <jahughes241@gmail.com>
2021-04-20 10:26:32 -0700

    ✨ Display changes to incompatable settings
    For example, zoomButtons disables lockViewport. This gets reflected in 
    the UI

--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
 import { Calc } from "desmodder";
 import { Config, configList } from "./config";
 
+type ConfigOptional = {
+  [K in keyof Config]?: Config[K];
+};
+
 const managedKeys = configList.map((e) => e.key);
 
 let initialSettings: null | Config = null;
@@ -26,9 +30,32 @@ export default {
   onDisable: onDisable,
   enabledByDefault: false,
   config: configList,
-  onConfigChange<K extends keyof Config>(key: K, value: Config[K]) {
-    Calc.updateSettings({
-      [key]: value,
-    });
+  onConfigChange(changes: ConfigOptional) {
+    // called only when plugin is active
+    Calc.updateSettings(changes);
+  },
+  manageConfigChange(current: Config, changes: ConfigOptional) {
+    const proposedConfig = {
+      ...current,
+      ...changes,
+    };
+    const newChanges = {
+      ...changes,
+    };
+    if (changes.zoomButtons) {
+      if (false === proposedConfig.graphpaper) {
+        newChanges.graphpaper = true;
+      }
+      if (proposedConfig.lockViewport) {
+        newChanges.lockViewport = false;
+      }
+    }
+    if (changes.lockViewport && proposedConfig.zoomButtons) {
+      newChanges.zoomButtons = false;
+    }
+    if (false === changes.graphpaper && proposedConfig.zoomButtons) {
+      newChanges.zoomButtons = false;
+    }
+    return newChanges;
   },
 };
