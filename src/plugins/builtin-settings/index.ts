DesModder/DesModder

commit 809397df0f731a76ed0282b0792220a96bce4655

🐛 Override settings from query parameters (#144)
They apply temporarily until page refresh or until the the options are
re-toggled within the extension settings

src/plugins/builtin-settings/index.ts
@@ -1,4 +1,4 @@
-import { Calc, OptionalProperties } from "desmodder";
+import { Calc, OptionalProperties, getQueryParams } from "desmodder";
 import { Config, configList } from "./config";
 
 type ConfigOptional = OptionalProperties<Config>;
@@ -7,12 +7,47 @@ const managedKeys = configList.map((e) => e.key);
 
 let initialSettings: null | Config = null;
 
+function manageConfigChange(current: Config, changes: ConfigOptional) {
+  const proposedConfig = {
+    ...current,
+    ...changes,
+  };
+  const newChanges = {
+    ...changes,
+  };
+  if (changes.zoomButtons) {
+    if (false === proposedConfig.graphpaper) {
+      newChanges.graphpaper = true;
+    }
+    if (proposedConfig.lockViewport) {
+      newChanges.lockViewport = false;
+    }
+  }
+  if (changes.lockViewport && proposedConfig.zoomButtons) {
+    newChanges.zoomButtons = false;
+  }
+  if (false === changes.graphpaper && proposedConfig.zoomButtons) {
+    newChanges.zoomButtons = false;
+  }
+  return newChanges;
+}
+
 function onEnable(config: Config) {
   initialSettings = { ...config };
+  const queryParams = getQueryParams();
   for (const key of managedKeys) {
     initialSettings[key] = Calc.settings[key];
   }
-  Calc.updateSettings(config);
+  const queryConfig: ConfigOptional = {};
+  for (const key of managedKeys) {
+    if (queryParams[key]) {
+      queryConfig[key] = true;
+    }
+    if (queryParams["no" + key]) {
+      queryConfig[key] = false;
+    }
+  }
+  Calc.updateSettings(manageConfigChange(config, queryConfig));
 }
 
 function onDisable() {
@@ -35,28 +70,5 @@ export default {
     // called only when plugin is active
     Calc.updateSettings(changes);
   },
-  manageConfigChange(current: Config, changes: ConfigOptional) {
-    const proposedConfig = {
-      ...current,
-      ...changes,
-    };
-    const newChanges = {
-      ...changes,
-    };
-    if (changes.zoomButtons) {
-      if (false === proposedConfig.graphpaper) {
-        newChanges.graphpaper = true;
-      }
-      if (proposedConfig.lockViewport) {
-        newChanges.lockViewport = false;
-      }
-    }
-    if (changes.lockViewport && proposedConfig.zoomButtons) {
-      newChanges.zoomButtons = false;
-    }
-    if (false === changes.graphpaper && proposedConfig.zoomButtons) {
-      newChanges.zoomButtons = false;
-    }
-    return newChanges;
-  },
+  manageConfigChange,
 } as const;

src/desmodder.ts
@@ -46,5 +46,6 @@ export {
   keys,
   promisify,
   OptionalProperties,
+  getQueryParams,
 } from "utils";
 export { controller as desModderController } from "./script";

src/utils.tsx
@@ -71,3 +71,6 @@ export const promisify =
 export type OptionalProperties<T> = {
   [K in keyof T]?: T[K];
 };
+
+export const getQueryParams: () => { [key: string]: string | true } =
+  desmosRequire("lib/parse-query-params").getQueryParams;

commit f4a70d6578fdeef490961879e668042a1087f053

🐛 Fix builtin-settings extension storage being ignored
Broken in #144. Now extension storage correctly affects the settings,
but they *can* be overriden by query parameters

src/plugins/builtin-settings/index.ts
@@ -47,7 +47,11 @@ function onEnable(config: Config) {
       queryConfig[key] = false;
     }
   }
-  Calc.updateSettings(manageConfigChange(config, queryConfig));
+  const newChanges = manageConfigChange(config, queryConfig);
+  Calc.updateSettings({
+    ...config,
+    ...newChanges,
+  });
 }
 
 function onDisable() {