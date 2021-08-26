9e953a27df08eb5def896242f16ce5d67acf0c27
Jared Hughes <jahughes241@gmail.com>
2021-08-25 22:16:43 -0700

    Run prettier

--- src/plugins/GLesmos/exportAsGLesmos.ts
+++ src/plugins/GLesmos/exportAsGLesmos.ts
@@ -4,18 +4,29 @@ import Expression, { ChildExprNode } from "parsing/parsenode";
 type GLesmosExprCategory = "function" | "expression" | "unimplemented";
 
 function glslFloatify(x: number) {
-    return Number.isInteger(x) ? x.toString()+".0" : x.toString();
+  return Number.isInteger(x) ? x.toString() + ".0" : x.toString();
 }
 
 export default function exportAsGLesmos() {
   const state = Calc.getState();
   const glSnippets = state.expressions.list.map(exprToGL);
-  return glSnippets.filter(s => s[1] == "function").map(s=>s[0]).join("\n\n") + 
-  `\n\nvec4 outColor = vec4(1.0);\n\n`+
-  `void glesmosMain(vec2 coords) { float x = coords.x; float y = coords.y;\n\n    ` +
-  glSnippets.filter(s => s[1] == "expression").map(s=>s[0]).join("\n    ") + 
-  "\n\n}\n\n" +
-  glSnippets.filter(s => s[1] == "unimplemented").map(s=>s[0]).join("\n\n");
+  return (
+    glSnippets
+      .filter((s) => s[1] == "function")
+      .map((s) => s[0])
+      .join("\n\n") +
+    `\n\nvec4 outColor = vec4(1.0);\n\n` +
+    `void glesmosMain(vec2 coords) { float x = coords.x; float y = coords.y;\n\n    ` +
+    glSnippets
+      .filter((s) => s[1] == "expression")
+      .map((s) => s[0])
+      .join("\n    ") +
+    "\n\n}\n\n" +
+    glSnippets
+      .filter((s) => s[1] == "unimplemented")
+      .map((s) => s[0])
+      .join("\n\n")
+  );
 }
 
 function exprToGL(expr: ItemModel): [string, GLesmosExprCategory] {
@@ -26,27 +37,44 @@ function exprToGL(expr: ItemModel): [string, GLesmosExprCategory] {
       const parsed = parseDesmosLatex(latex);
       switch (parsed.type) {
         case "FunctionDefinition":
-          return [(
-            `float ${parsed._symbol}(${parsed._argSymbols.map(s => "float "+s).join(", ")}) {\n` +
-            `  return ${childExprToGL(parsed._expression)};\n` +
-            `}`
-          ), "function"];
+          return [
+            `float ${parsed._symbol}(${parsed._argSymbols
+              .map((s) => "float " + s)
+              .join(", ")}) {\n` +
+              `  return ${childExprToGL(parsed._expression)};\n` +
+              `}`,
+            "function",
+          ];
         case "Assignment":
-          return [`float ${parsed._symbol} = ${childExprToGL(parsed._expression)};`, "expression"];
+          return [
+            `float ${parsed._symbol} = ${childExprToGL(parsed._expression)};`,
+            "expression",
+          ];
         case "Comparator['<']":
           let col = expr.color ? expr.color : "#00FF00";
           let r = glslFloatify(parseInt(col.slice(1, 3), 16) / 256);
           let g = glslFloatify(parseInt(col.slice(3, 5), 16) / 256);
           let b = glslFloatify(parseInt(col.slice(5, 7), 16) / 256);
-          let a = (expr.fillOpacity === undefined) ? 0.4 : expr.fillOpacity;
-          return [`if (${childExprToGL(parsed.args[0] as ChildExprNode)} - ${childExprToGL(parsed.args[1] as ChildExprNode)} < 0.0) {\n`+
-          `    outColor.rgb = mix(outColor.rgb, vec3(${r}, ${g}, ${b}), ${a});\n`+
-          `}`, "expression"]
+          let a = expr.fillOpacity === undefined ? 0.4 : expr.fillOpacity;
+          return [
+            `if (${childExprToGL(
+              parsed.args[0] as ChildExprNode
+            )} - ${childExprToGL(parsed.args[1] as ChildExprNode)} < 0.0) {\n` +
+              `    outColor.rgb = mix(outColor.rgb, vec3(${r}, ${g}, ${b}), ${a});\n` +
+              `}`,
+            "expression",
+          ];
         default:
-          return [`// Unimplemented root level type: ${parsed.type}`, "unimplemented"];
+          return [
+            `// Unimplemented root level type: ${parsed.type}`,
+            "unimplemented",
+          ];
       }
     default:
-      return [`// Unimplemented item model type: ${expr.type}`, "unimplemented"];
+      return [
+        `// Unimplemented item model type: ${expr.type}`,
+        "unimplemented",
+      ];
   }
 }
 
@@ -87,7 +115,7 @@ function childExprToGL(expr: ChildExprNode): string {
     case "FunctionCall":
       return `${expr._symbol}(${expr.args.map(childExprToGL).join(", ")})`;
     case "Comparator['<']":
-      return `${childExprToGL(expr._difference())} < 0.0`
+      return `${childExprToGL(expr._difference())} < 0.0`;
     default:
       throw `Unimplemented subexpression type: ${expr.type}`;
   }
