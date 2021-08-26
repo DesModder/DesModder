a4b98df25aae20502160333f4cdcf9b3c643aeec
radian628 <break.a.radian@gmail.com>
2021-08-25 21:44:30 -0700

    added glesmos canvas + inequality renderer for <
    - glesmos canvas preview appears in bottom right corner
    - renders inequalities (must be manually done in console)
    - fixed glsl errors

--- src/plugins/GLesmos/exportAsGLesmos.ts
+++ src/plugins/GLesmos/exportAsGLesmos.ts
@@ -1,31 +1,52 @@
 import { Calc, ItemModel, parseDesmosLatex } from "desmodder";
 import Expression, { ChildExprNode } from "parsing/parsenode";
 
-export default function exportAsGLesmos() {
-  const state = Calc.getState();
-  return state.expressions.list.map(exprToGL).join("\n\n");
+type GLesmosExprCategory = "function" | "expression" | "unimplemented";
+
+function glslFloatify(x: number) {
+    return Number.isInteger(x) ? x.toString()+".0" : x.toString();
 }
 
-function exprToGL(expr: ItemModel): string {
+export default function exportAsGLesmos() {
+  const state = Calc.getState();
+  const glSnippets = state.expressions.list.map(exprToGL);
+  return glSnippets.filter(s => s[1] == "function").map(s=>s[0]).join("\n\n") + 
+  `\n\nvec4 outColor = vec4(1.0);\n\n`+
+  `void glesmosMain(vec2 coords) { float x = coords.x; float y = coords.y;\n\n    ` +
+  glSnippets.filter(s => s[1] == "expression").map(s=>s[0]).join("\n    ") + 
+  "\n\n}\n\n" +
+  glSnippets.filter(s => s[1] == "unimplemented").map(s=>s[0]).join("\n\n");
+}
+
+function exprToGL(expr: ItemModel): [string, GLesmosExprCategory] {
   switch (expr.type) {
     case "expression":
       const latex = expr.latex;
-      if (latex === undefined) return "";
+      if (latex === undefined) return ["", "unimplemented"];
       const parsed = parseDesmosLatex(latex);
       switch (parsed.type) {
         case "FunctionDefinition":
-          return (
-            `${parsed._symbol}(${parsed._argSymbols.join(", ")}) {\n` +
+          return [(
+            `float ${parsed._symbol}(${parsed._argSymbols.map(s => "float "+s).join(", ")}) {\n` +
             `  return ${childExprToGL(parsed._expression)};\n` +
             `}`
-          );
+          ), "function"];
         case "Assignment":
-          return `${parsed._symbol} = ${childExprToGL(parsed._expression)}`;
+          return [`float ${parsed._symbol} = ${childExprToGL(parsed._expression)};`, "expression"];
+        case "Comparator['<']":
+          let col = expr.color ? expr.color : "#00FF00";
+          let r = glslFloatify(parseInt(col.slice(1, 3), 16) / 256);
+          let g = glslFloatify(parseInt(col.slice(3, 5), 16) / 256);
+          let b = glslFloatify(parseInt(col.slice(5, 7), 16) / 256);
+          let a = (expr.fillOpacity === undefined) ? 0.4 : expr.fillOpacity;
+          return [`if (${childExprToGL(parsed.args[0] as ChildExprNode)} - ${childExprToGL(parsed.args[1] as ChildExprNode)} < 0.0) {\n`+
+          `    outColor.rgb = mix(outColor.rgb, vec3(${r}, ${g}, ${b}), ${a});\n`+
+          `}`, "expression"]
         default:
-          return `// Unimplemented root level type: ${parsed.type}`;
+          return [`// Unimplemented root level type: ${parsed.type}`, "unimplemented"];
       }
     default:
-      return `// Unimplemented item model type: ${expr.type}`;
+      return [`// Unimplemented item model type: ${expr.type}`, "unimplemented"];
   }
 }
 
@@ -40,9 +61,9 @@ function childExprToGL(expr: ChildExprNode): string {
       if (typeof num === "boolean") {
         return num ? "true" : "false";
       } else if (typeof num === "number") {
-        return num.toString();
+        return glslFloatify(num);
       } else {
-        return (num.n / num.d).toString();
+        return glslFloatify(num.n / num.d);
       }
     case "Add":
     case "Multiply":
@@ -65,6 +86,8 @@ function childExprToGL(expr: ChildExprNode): string {
       return `-(${childExprToGL(expr.args[0])})`;
     case "FunctionCall":
       return `${expr._symbol}(${expr.args.map(childExprToGL).join(", ")})`;
+    case "Comparator['<']":
+      return `${childExprToGL(expr._difference())} < 0.0`
     default:
       throw `Unimplemented subexpression type: ${expr.type}`;
   }
