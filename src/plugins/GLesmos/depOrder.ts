b56c5c4de0a5a2626f263a2b879ac79f17940afe
Jared Hughes <jahughes241@gmail.com>
2021-08-30 22:09:13 -0700

    Handle GLesmos dependency orders (#200)
    Tested with https://www.desmos.com/calculator/5st28xna0v

---
+++ src/plugins/GLesmos/depOrder.ts
@@ -0,0 +1,76 @@
+import { Assignment, FunctionDefinition } from "parsing/parsenode";
+import { ComputedContext } from "./computeContext";
+
+export function orderDeps(context: ComputedContext, implicitIDs: string[]) {
+  // topological order
+  // assumes no cyclic dependencies (Desmos deal with this for us
+  // by setting context.frame[id].type === "Error" instead of "FunctionDefinition")
+
+  /*
+  Suppose we have
+    [implicit:] 0 = f(y)
+    f(x) = g(x) · h(x)
+    h(x) = 1 + g(x)
+    g(x) = sqrt(x)
+  Then we will push, in this order:
+    f, (from implicit)
+    g, h (from f)
+    g (from h)
+  We output in reversed order, with duplicates removed: g,h,f
+  */
+
+  let funcsWithDuplicates = [];
+  let varsWithDuplicates = [];
+  for (let id of implicitIDs) {
+    const analysis = context.analysis[id];
+    // appease type checker; getImplicits() already excludes errors
+    if (analysis.rawTree.type === "Error") continue;
+
+    let deps = analysis.rawTree.getDependencies();
+    let d;
+    while ((d = deps.pop()) !== undefined) {
+      let frameDep = context.frame[d];
+      if (frameDep === undefined) {
+        // d is probably a free variable like "x" or "y"
+        // ignore
+      } else if (!("type" in frameDep)) {
+        // frameDep is {isFunction: true}
+        // this is a built-in like sin or abs
+        // ignore
+      } else {
+        if (frameDep?.type === "FunctionDefinition") {
+          funcsWithDuplicates.push(d);
+          deps.push(...frameDep.getDependencies());
+        } else if (frameDep?.type === "Assignment") {
+          varsWithDuplicates.push(d);
+          deps.push(...frameDep.getDependencies());
+        } else if (frameDep?.type === "Constant") {
+          // this is e, pi, tau, infty, or trigAngleMultiplier
+          // ignore
+        } else {
+          throw `Dependency is of type ${frameDep.type}, but it is expected to be FunctionDefinition or Assignment`;
+        }
+      }
+    }
+  }
+
+  return {
+    funcs: reverseUnique(funcsWithDuplicates).map(
+      (c) => context.frame[c] as FunctionDefinition
+    ),
+    vars: reverseUnique(varsWithDuplicates).map(
+      (c) => context.frame[c] as Assignment
+    ),
+  };
+}
+
+function reverseUnique(a: string[]) {
+  let ordered: string[] = [];
+  for (let i = a.length - 1; i >= 0; i--) {
+    let c = a[i];
+    if (!ordered.includes(c)) {
+      ordered.push(c);
+    }
+  }
+  return ordered;
+}
