e4eca512a485caca4a8a1e247f541a59ba3a885d
Jared Hughes <jahughes241@gmail.com>
2021-10-13 01:17:27 -0700

    Improve tips UI and allow links

--- src/plugins/show-tips/Tip.tsx
+++ src/plugins/show-tips/Tip.tsx
@@ -1,14 +1,25 @@
-import { Calc, DCGView } from "desmodder";
-import tips from "./tips";
+import { Calc, DCGView, If, jquery } from "desmodder";
+import tips, { TipData } from "./tips";
 import "./Tip.less";
 
 export default class Tip extends DCGView.Class {
-  currentTip: string = this.randomTip();
+  currentTip: TipData = this.randomTip();
 
   template() {
     return (
       <div class="dsm-usage-tip" onTap={() => this.nextTip()}>
-        {() => this.currentTip}
+        <div>{() => this.currentTip.desc}</div>
+        <If predicate={() => this.currentTip?.learnMore !== undefined}>
+          {() => (
+            <a
+              href={() => this.currentTip?.learnMore}
+              target="_blank"
+              onTap={(e: MouseEvent) => e.stopPropagation()}
+            >
+              Learn more
+            </a>
+          )}
+        </If>
       </div>
     );
   }
@@ -18,12 +29,16 @@ export default class Tip extends DCGView.Class {
   }
 
   nextTip() {
-    this.currentTip = this.randomTip();
+    let nextTip = this.randomTip();
+    while (nextTip === this.currentTip) {
+      // Avoid repeating the same tip two times in a row
+      nextTip = this.randomTip();
+    }
+    this.currentTip = nextTip;
     Calc.controller.updateViews();
   }
 }
 
 export function createTipElement() {
-  console.log("creating tip element");
   return <Tip></Tip>;
 }
--- src/plugins/show-tips/tips.ts
+++ src/plugins/show-tips/tips.ts
@@ -1,29 +1,110 @@
-export default [
+export interface TipData {
+  desc: string;
+  learnMore?: string;
+}
+
+const tips: TipData[] = [
   /* DesModder features */
-  "When exporting videos, prefer MP4 or APNG over GIF",
-  "Disabling graphpaper in Calculator Settings is useful for writing a sequence of equations",
-  "Paste ASCII Math such as Wolfram Alpha output directly into Desmos",
-  "Pin (bookmark) commonly-used sliders and actions for easy access",
-  "It's commonly a good idea to test the first few frames of a video before starting a long capture",
-  "Find and Replace is great for renaming variables",
-  "Press Ctrl+Q to duplicate the current expression",
-  "Type Shift+Enter inside notes and folder titles to type a newline",
-  "Click the yellow triangle (or type shift+enter) to fade a warning and hide sliders",
-  /* Desmos tips */ // TODO: DEMO for most
-  'Type `"` to quickly start a note or "folder" to make a folder',
-  "Use arctan(y,x) instead of arctan(y/x) to get the angle of a point",
-  "Integrals can have infinite bounds",
-  "You can take random samples from a distribution like normaldist().random()",
-  "Round to a certain number of decimals using round(1.1234, 2)",
-  "Sort one list using keys of another list with sort(A,B)",
-  "Create custom colors using `v = rgb(255,255,255)` or `v_{2} = hsv(100,0.8,1)`",
-  "Press Ctrl+F to search",
-  "Take derivatives using prime notation or Leibniz notation",
-  "Drop the first element of a list by using `L[2...]`",
-  "View a summary of data using stats(L), or get more detail with histogram(L)",
-  "Fit a bunch of data points using `y_{1} ~ a*x_{1}+b`",
-  "Use the polygon function for easy polygons",
-  "Point (vector) arithmetic works as expected",
+  {
+    desc: "When exporting videos, prefer MP4 or APNG over GIF",
+  },
+  {
+    desc: "Disabling graphpaper in Calculator Settings is useful for writing a sequence of equations",
+  },
+  {
+    desc: "Paste ASCII Math directly into Desmos",
+  },
+  {
+    desc: "Pin (bookmark) commonly-used sliders and actions for easy access",
+  },
+  {
+    desc: "Before starting a long capture, it's safest to test the beginning of an export",
+  },
+  {
+    desc: "Find and Replace is great for renaming variables",
+  },
+  {
+    desc: "Press Ctrl+Q to duplicate the current expression",
+  },
+  {
+    desc: "Type Shift+Enter inside notes and folder titles for a newline",
+  },
+  {
+    desc: "Click the yellow triangle (or type shift+enter) to fade a warning and hide sliders",
+  },
+  /* Desmos tips */
+  {
+    desc: 'Type `"` to quickly make a note or "folder" for a folder',
+  },
+  {
+    desc: "Use arctan(y,x) instead of arctan(y/x) to get the angle of a point",
+    learnMore: "https://www.desmos.com/calculator/beqk9rnxbl",
+  },
+  {
+    desc: "Integrals can have infinite bounds",
+    learnMore:
+      "https://help.desmos.com/hc/en-us/articles/4406810279693-Integrals",
+  },
+  {
+    desc: "The random function can sample from a distribution",
+    learnMore:
+      "https://help.desmos.com/hc/en-us/articles/4405633253389-Statistics",
+  },
+  {
+    desc: "Two-argument round is great for rounding labels",
+    learnMore: "https://www.desmos.com/calculator/wnm3fwctnf",
+  },
+  {
+    desc: "Sort one list using keys of another list with sort(A,B)",
+    learnMore: "https://www.desmos.com/calculator/ul4isx0qiz",
+  },
+  {
+    desc: "Create custom colors and re-use them using variables",
+    learnMore:
+      "https://help.desmos.com/hc/en-us/articles/4406795899533-Custom-Colors",
+  },
+  {
+    desc: "Press Ctrl+F to search through expressions",
+  },
+  {
+    desc: "Take derivatives using prime notation or Leibniz notation",
+    learnMore:
+      "https://help.desmos.com/hc/en-us/articles/4406809433613-Derivatives",
+  },
+  {
+    desc: "List slices do not have to be bounded",
+    learnMore: "https://www.desmos.com/calculator/4z0wllsipm",
+  },
+  {
+    desc: "To view a summmary of data, use the `histogram` or `stats` functions",
+    learnMore:
+      "https://help.desmos.com/hc/en-us/articles/4405633253389-Statistics",
+  },
+  {
+    desc: "Use a table for a list of draggable points",
+  },
+  {
+    desc: "Use the polygon function for easy polygons",
+    learnMore:
+      "https://help.desmos.com/hc/en-us/articles/4405488514573-Polygons",
+  },
+  {
+    desc: "Point (vector) arithmetic works as expected",
+  },
+  {
+    desc: "Shift + mouse drag over an axis to scale only that axis",
+  },
+  {
+    desc: "Use actions and tickers to run simulations",
+    learnMore: "https://help.desmos.com/hc/en-us/articles/4407725009165",
+  },
+  {
+    desc: "The math from Desmos can be directly copy-pasted into LaTeX editors",
+  },
   /* Motivation */
-  "You're doing great :)",
+  {
+    desc: "You're doing great :)",
+  },
 ];
+
+export default tips;
