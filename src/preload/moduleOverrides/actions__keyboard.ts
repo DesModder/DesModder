979a11886554d9a7ffd27a2facaec58afc59bd48
Jared Hughes <jahughes241@gmail.com>
2021-10-18 16:25:22 -0700

    Fix #245: down arrow moves cursor to end of expressions
    The offending module override is no longer necessary; it is vestigial from preventing down arrow from moving the cursor to the next expression.

--- src/preload/moduleOverrides/actions__keyboard.ts
+++ src/preload/moduleOverrides/actions__keyboard.ts
@@ -2,32 +2,6 @@ import * as t from "@babel/types";
 import template from "@babel/template";
 
 export default () => ({
-  CallExpression(path: babel.NodePath<t.CallExpression>) {
-    /* @plugin pin-expressions
-    
-    @what Prevent the down arrow from creating a new last item when pressing down from the bottom-most pinned expression
-    
-    @how
-      Replaces
-          !List.selectNextItem(e.getListModel())
-      with
-        !(window.DesModder?.controller?.isPinned(e.getSelectedItem().id) + List.selectNextItem(e.getListModel()))
-    */
-    if (
-      t.isMemberExpression(path.node.callee) &&
-      t.isIdentifier(path.node.callee.property, { name: "selectNextItem" })
-    ) {
-      path.replaceWith(
-        template.expression(
-          `window.DesModder?.controller?.isPinned(%%e%%.getSelectedItem().id) + %%callSelectNextItem%%`
-        )({
-          callSelectNextItem: path.node,
-          e: path.getFunctionParent()?.node.params[0],
-        })
-      );
-      path.skip();
-    }
-  },
   ReturnStatement(path: babel.NodePath<t.ReturnStatement>) {
     /* @plugin hide-errors
 
