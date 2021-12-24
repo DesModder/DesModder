615611a7556a7506fff96f8680be6bfaa3be5bbd
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-12-23 16:00:01 -0800

    Fix + secure implementation of background heartbeat sending

--- src/background.ts
+++ src/background.ts
@@ -4,27 +4,31 @@ chrome.action.onClicked.addListener(() => {
   });
 });
 
-export type FetchMessage = {
-  type: "fetch";
-  url: string;
+export type HearbeatMessage = {
+  type: "sendHeartbeat";
   options: RequestInit | undefined;
 };
-export type RuntimeMessage = FetchMessage;
+export type RuntimeMessage = HearbeatMessage;
 
 export type ErrorResponse = { type: "error"; message: string };
 export type SuccessResponse = { type: "success" };
 export type RuntimeResponse = ErrorResponse | SuccessResponse;
 
-async function handleFetch(
-  req: FetchMessage,
+async function sendHeartbeat(
+  req: HearbeatMessage,
   sendResponse: (res: RuntimeResponse) => void
 ) {
-  const r = await fetch(req.url, req.options);
-  // for now we don't care about the response.
-  if (r.status != 200) {
+  const r = await fetch(
+    "https://wakatime.com/api/v1/users/current/heartbeats",
+    req.options
+  );
+  // for now we don't care about the response content, just the status.
+  if (r.status != 201) {
     const res: ErrorResponse = {
       type: "error",
-      message: `Request failed with status ${r.status}`,
+      message:
+        `Request failed with status ${r.status} (expected 201).` +
+        "Check background page network tab for details.",
     };
     return sendResponse(res);
   }
@@ -33,16 +37,18 @@ async function handleFetch(
 }
 
 // Send requests that would otherwise be blocked by CORS if sent from a content script
-chrome.runtime.onMessage.addListener(async (msg, sender, sendResponse) => {
-  const req = msg as RuntimeMessage;
-  try {
-    if (req.type == "fetch") {
-      await handleFetch(req, sendResponse);
+chrome.runtime.onMessageExternal.addListener(
+  async (msg, sender, sendResponse) => {
+    const req = msg as RuntimeMessage;
+    try {
+      if (req.type == "sendHeartbeat") {
+        await sendHeartbeat(req, sendResponse);
+      }
+    } catch (e: any) {
+      console.error(e);
+      // if a response has already been sent this will be a no-op
+      const res: ErrorResponse = { type: "error", message: e?.message ?? "" };
+      sendResponse(res);
     }
-  } catch (e: any) {
-    console.error(e);
-    // if a response has already been sent this will be a no-op
-    const res: ErrorResponse = { type: "error", message: e?.message ?? "" };
-    sendResponse(res);
   }
-});
+);
