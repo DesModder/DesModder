4737ac26cb647403e9a097a1bf370d08a5ba90de
Jared Hughes <jahughes241@gmail.com>
2022-05-28 20:22:56 -0700

    Apply updates from graph changes

--- src/plugins/text-mode/Controller.ts
+++ src/plugins/text-mode/Controller.ts
@@ -2,10 +2,12 @@ import { EditorView, ViewUpdate } from "@codemirror/view";
 import { Calc } from "globals/window";
 import { initView } from "./view/editor";
 import applyText from "./down/applyText";
+import getText from "./up/getText";
 
 export default class Controller {
   inTextMode: boolean = false;
   view: EditorView | null = null;
+  applyingUpdateFromGraph: boolean = false;
 
   toggleTextMode() {
     this.inTextMode = !this.inTextMode;
@@ -15,9 +17,11 @@ export default class Controller {
   mountEditor(container: HTMLDivElement) {
     this.view = initView(this);
     container.appendChild(this.view.dom);
+    Calc.observeEvent("change.dsm-text-mode", () => this.updateFromGraph());
   }
 
   unmountEditor(container: HTMLDivElement) {
+    Calc.unobserveEvent("change.dsm-text-mode");
     if (this.view) {
       this.view.destroy();
       this.view = null;
@@ -25,10 +29,26 @@ export default class Controller {
   }
 
   handleUpdate(update: ViewUpdate) {
-    if (this.view && update.docChanged) {
+    if (!this.applyingUpdateFromGraph && this.view && update.docChanged) {
       const text = this.view.state.sliceDoc();
       applyText(text);
       this.view.focus();
+    }
+  }
+
+  updateFromGraph() {
+    // Update changes from user dragging point, slider/regression tick, etc.
+    // TODO: incremental update
+    if (this.view) {
+      this.applyingUpdateFromGraph = true;
+      const editorState = this.view.state;
+      const text = getText();
+      this.view.update([
+        editorState.update({
+          changes: { from: 0, to: editorState.doc.length, insert: text },
+        }),
+      ]);
+      this.applyingUpdateFromGraph = false;
     }
   }
 }
