fb80f52edc63a89d363eb2e8fee1268f3bdc4165
Jared Hughes <jahughes241@gmail.com>
2022-05-28 20:50:04 -0700

    Avoid duplicated text update on graph change

--- src/plugins/text-mode/Controller.ts
+++ src/plugins/text-mode/Controller.ts
@@ -8,6 +8,7 @@ export default class Controller {
   inTextMode: boolean = false;
   view: EditorView | null = null;
   applyingUpdateFromGraph: boolean = false;
+  applyingUpdateFromText: boolean = false;
 
   toggleTextMode() {
     this.inTextMode = !this.inTextMode;
@@ -17,7 +18,9 @@ export default class Controller {
   mountEditor(container: HTMLDivElement) {
     this.view = initView(this);
     container.appendChild(this.view.dom);
-    Calc.observeEvent("change.dsm-text-mode", () => this.updateFromGraph());
+    Calc.observeEvent("change.dsm-text-mode", () =>
+      setTimeout(() => this.updateFromGraph(), 1000)
+    );
   }
 
   unmountEditor(container: HTMLDivElement) {
@@ -30,16 +33,18 @@ export default class Controller {
 
   handleUpdate(update: ViewUpdate) {
     if (!this.applyingUpdateFromGraph && this.view && update.docChanged) {
+      this.applyingUpdateFromText = true;
       const text = this.view.state.sliceDoc();
       applyText(text);
       this.view.focus();
+      this.applyingUpdateFromText = false;
     }
   }
 
   updateFromGraph() {
     // Update changes from user dragging point, slider/regression tick, etc.
     // TODO: incremental update
-    if (this.view) {
+    if (!this.applyingUpdateFromText && this.view) {
       this.applyingUpdateFromGraph = true;
       const editorState = this.view.state;
       const text = getText();
