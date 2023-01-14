adcd120e8cdeba5d0c7ec241c0fb16fb8269733a
Jared Hughes <jahughes241@gmail.com>
2023-01-14 02:19:49 -0800

    Fix debug mode

--- src/plugins/debug-mode/index.ts
+++ src/plugins/debug-mode/index.ts
@@ -4,10 +4,14 @@ import { Plugin } from "plugins";
 const debugMode: Plugin = {
   id: "debug-mode",
   onEnable: () => {
+    // The displayed indexes are stored in some state somewhere, so
+    // update the state first before updating views
+    Calc.controller.updateTheComputedWorld();
     Calc.controller.updateViews();
   },
   onDisable: () => {},
   afterDisable: () => {
+    Calc.controller.updateTheComputedWorld();
     Calc.controller.updateViews();
   },
   alwaysEnabled: false,
