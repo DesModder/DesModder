6e1a2fe54d97c5dfb7110bfafffdb48903c308b9
Jared Hughes <jahughes241@gmail.com>
2023-01-18 13:45:51 -0800

    Add esbuild args (watch mode, firefox build)

--- esbuild.mjs
+++ esbuild.mjs
@@ -1,12 +1,48 @@
 import { esbuildPluginLezer } from "./loaders/esbuild-plugin-lezer.mjs";
 import { esbuildPluginReplacements } from "./loaders/esbuild-plugin-replacements.mjs";
-import { build } from "esbuild";
+import esbuild from "esbuild";
 import { copy } from "esbuild-plugin-copy";
 import { lessLoader } from "esbuild-plugin-less";
+import { promises as fs } from "fs";
+import parseArgs from "minimist-lite";
 
-const browser = "chrome";
+const argv = parseArgs(process.argv.slice(2));
 
-build({
+const unindent2spaces = (s) => s.toString().replaceAll(/\n {2}|\n\s*$/g, "\n");
+
+if (argv.help) {
+  console.log(
+    unindent2spaces`Usage:
+    node esbuild.mjs [options]
+  
+  Options:
+    --help         Show help
+    --browser=...  Browser target: "firefox", or "chrome" (default)
+    --watch        Watch mode: rebuild on file system changes
+    --clean        Clear output directory
+    
+  Examples:
+    # Dev server for Chrome
+    node esbuild.mjs --watch --browser=chrome
+    
+    # Final build for Firefox
+    node esbuild.mjs --clean --browser=firefox`
+  );
+  process.exit(0);
+}
+
+if (
+  argv.browser !== undefined &&
+  !["firefox", "chrome"].includes(argv.browser)
+) {
+  console.error(`Invalid browser name: ${argv.browser}`);
+  process.exit(1);
+}
+const browser = argv.browser ?? "chrome";
+const watch = !!argv.watch;
+const clean = !!argv.clean;
+
+const opts = {
   entryPoints: [
     "src/background.ts",
     "src/script.ts",
@@ -46,4 +82,22 @@ build({
     ".ftl": "text",
     ".woff": "dataurl",
   },
-});
+  logLevel: "info",
+};
+
+if (clean) {
+  try {
+    await fs.rm("dist", { recursive: true });
+  } catch (e) {
+    // permit no dist folder to begin with
+    if (e?.code !== "ENOENT") throw e;
+  }
+}
+
+if (watch) {
+  const ctx = await esbuild.context(opts);
+  await ctx.rebuild();
+  await ctx.watch();
+} else {
+  esbuild.build(opts);
+}
