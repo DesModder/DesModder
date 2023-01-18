9c07ef6111ae635b545926ed4df4a2e7bb468317
Jared Hughes <jahughes241@gmail.com>
2023-01-17 18:27:08 -0800

    Fix toggling of show-tips

--- src/plugins/show-tips/index.ts
+++ src/plugins/show-tips/index.ts
@@ -1,13 +1,19 @@
 import { Calc } from "globals/window";
 import { Plugin } from "plugins";
 
+function apiContainer() {
+  return document.querySelector(".dcg-calculator-api-container");
+}
+
 const showTips: Plugin = {
   id: "show-tips",
   onEnable: () => {
+    apiContainer()?.classList.add("dsm-show-tips");
     Calc.controller.updateViews();
   },
   onDisable: () => {},
   afterDisable: () => {
+    apiContainer()?.classList.remove("dsm-show-tips");
     Calc.controller.updateViews();
   },
   enabledByDefault: true,
