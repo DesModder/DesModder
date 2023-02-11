0cac36d1e699a9bd3ee9b8c326b324a2bb30d117
Jared Hughes <jahughes241@gmail.com>
2023-02-10 22:10:39 -0800

    Remove try-catch around main init
    Fixes some type errors, and ensures that we only panic on plugin IDs.

--- src/script.ts
+++ src/script.ts
@@ -1,29 +1,23 @@
-import { addPanic } from "./panic/panic";
 import { pollForValue } from "./utils/utils";
 import "fonts/style.css";
 import window from "globals/window";
 import Controller from "main/Controller";
 import View from "main/View";
 
-let controller;
+const controller = new Controller();
 export { controller as desModderController };
 
-try {
-  controller = new Controller();
-  const view = new View();
+const view = new View();
 
-  window.DesModder = {
-    view,
-    controller,
-    exposedPlugins: controller.exposedPlugins,
-  };
-  void (async () => {
-    const pillbox = (await pollForValue(() =>
-      document.querySelector(".dcg-overgraph-pillbox-elements")
-    )) as HTMLElement;
-    controller.init(view);
-    view.init(controller, pillbox);
-  })();
-} catch (e) {
-  addPanic("DesModder");
-}
+window.DesModder = {
+  view,
+  controller,
+  exposedPlugins: controller.exposedPlugins,
+};
+void (async () => {
+  const pillbox = (await pollForValue(() =>
+    document.querySelector(".dcg-overgraph-pillbox-elements")
+  )) as HTMLElement;
+  controller.init(view);
+  view.init(controller, pillbox);
+})();
