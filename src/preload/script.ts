ff440c5ff13cd6a7c075fbb81d7cdb603a511b17
Jared Hughes <jahughes241@gmail.com>
2023-02-12 02:54:43 -0800

    Cache replacements for loading speed (#497)
    * Cache full-file replacements
    
    Brings Largest Contentful Paint (LCP) from ~3.5s to ~2s on reloads.
    
    * Cache the whole replaced file
    
    Previously caching worker separately, which needed parsing to find the position of the worker string.
    
    LCP down from ~2s to ~1.2s

--- src/preload/script.ts
+++ src/preload/script.ts
@@ -1,8 +1,6 @@
 import * as almond from "./almond";
-import moduleReplacements from "./moduleReplacements";
-import { applyReplacements } from "./replacementHelpers/applyReplacement";
+import { fullReplacementCached } from "./replacementHelpers/cacheReplacement";
 import window from "globals/window";
-import jsTokens from "js-tokens";
 import injectScript from "utils/injectScript";
 import { postMessageUp, listenToMessageDown } from "utils/messages";
 import { pollForValue } from "utils/utils";
@@ -65,50 +63,12 @@ void pollForValue(getCalcDesktopURL).then(async (srcURL: string) => {
   Now we load it, but with '?' appended to prevent the web request rules from blocking it */
   const calcDesktop = await (await fetch(srcURL + "?")).text();
   // Apply replacements
-  const newCode = applyReplacements(
-    moduleReplacements.filter((r) => !r.workerOnly),
-    calcDesktop
-  );
-  const newerCode = applyWorkerReplacements(newCode);
+  const newCode = await fullReplacementCached(calcDesktop, workerAppend);
   // tryRunDesModder polls until the following eval'd code is done.
   tryRunDesModder();
   // eslint-disable-next-line no-eval
-  (0, eval)(newerCode);
+  (0, eval)(newCode);
 });
-
-function applyWorkerReplacements(src: string): string {
-  // Apply replacements to the worker. This could also be done by tweaking the
-  // Worker constructor, but currently all of these replacements could be
-  // performed outside the main page
-  const tokens = Array.from(jsTokens(src));
-  const workerCodeTokens = tokens.filter(
-    (x) =>
-      x.type === "StringLiteral" &&
-      x.value.length > 200000 &&
-      // JS is sure to have &&. Protects against translations getting longer
-      // than the length cutoff, which is intentionally low in case of huge
-      // improvements in minification.
-      x.value.includes("&&")
-  );
-  if (workerCodeTokens.length > 1)
-    throw new Error("More than one worker code found");
-  const wcToken = workerCodeTokens[0];
-  wcToken.value = JSON.stringify(
-    // Place at the beginning of the code for the source mapping to line up
-    // Call at the end of the code to run after modules defined
-    `function loadDesModderWorker(){${workerAppend}\n}` +
-      applyReplacements(
-        moduleReplacements.filter((r) => r.workerOnly),
-        // JSON.parse doesn't work because this is a single-quoted string.
-        // js-tokens tokenized this as a string anyway, so it should be
-        // safely eval'able to a string.
-        // eslint-disable-next-line no-eval
-        (0, eval)(wcToken.value) as string
-      ) +
-      "\nloadDesModderWorker();"
-  );
-  return tokens.map((x) => x.value).join("");
-}
 
 listenToMessageDown((message) => {
   if (message.type === "set-worker-append-url") {
