6c07a0db42dc7fb7a762701df4e96e084776aa06
Jared Hughes <jahughes241@gmail.com>
2023-05-10 03:23:11 -0700

    Use transaction instead of nextEditDueToGraph

--- src/plugins/text-mode/LanguageServer.ts
+++ src/plugins/text-mode/LanguageServer.ts
@@ -14,6 +14,7 @@ import { Program, Statement } from "./down/TextAST";
 import textToRaw from "./down/textToRaw";
 import { RelevantEvent, eventSequenceChanges } from "./modify";
 import { Diagnostic } from "@codemirror/lint";
+import { Transaction } from "@codemirror/state";
 import { EditorView, ViewUpdate } from "@codemirror/view";
 import { GraphState } from "@desmodder/graph-state";
 
@@ -25,21 +26,12 @@ export interface ProgramAnalysis {
 
 export default class LanguageServer {
   analysis!: ProgramAnalysis;
-  /**
-   * Will the next onEdit be due to an update from the graph automatically
-   * changing the text, not a user?
-   *
-   * TODO: This should be replaced with the remote or userEvent annotations on
-   * transactions (in case an actual user event gets combined with this one),
-   * but I haven't figured out transaction annotations yet.
-   */
-  nextEditDueToGraph: boolean = true;
 
   constructor(
     private readonly view: EditorView,
     private readonly setCalcState: (state: GraphState) => void
   ) {
-    this.parse();
+    this.parse(false);
   }
 
   async doLint(): Promise<Diagnostic[]> {
@@ -48,18 +40,20 @@ export default class LanguageServer {
     });
   }
 
-  parse() {
+  parse(nextEditDueToGraph: boolean) {
     // Parse and set tree
     const s = this.view.state.doc.sliceString(0);
     const [analysis, rawGraphState] = textToRaw(s);
     this.analysis = analysis;
-    if (!this.nextEditDueToGraph && rawGraphState !== null)
+    if (!nextEditDueToGraph && rawGraphState !== null)
       this.setCalcState(rawGraphState);
-    this.nextEditDueToGraph = false;
   }
 
   onEditorUpdate(update: ViewUpdate) {
-    if (update.docChanged) this.parse();
+    if (update.docChanged)
+      this.parse(
+        update.transactions.every((x) => x.annotation(Transaction.remote))
+      );
   }
 
   /**
@@ -69,10 +63,10 @@ export default class LanguageServer {
   onCalcEvent(event: RelevantEvent) {
     const changes = eventSequenceChanges(this.view, [event], this.analysis);
     if (changes.length === 0) return;
-    const transaction = this.view.state.update({ changes });
-    // TODO: figure out annotations on this transaction as mentioned in the
-    // nextEditDueToGraph doc text
-    this.nextEditDueToGraph = true;
-    this.view.update([transaction]);
+    const transaction = this.view.state.update({
+      changes,
+      annotations: [Transaction.remote.of(true)],
+    });
+    this.view.dispatch(transaction);
   }
 }
