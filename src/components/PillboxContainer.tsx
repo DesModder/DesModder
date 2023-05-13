816b887779565717592f739d4dca8571d257bdd5
Jared Hughes <jahughes241@gmail.com>
2023-05-13 15:32:58 -0700

    Move pillbox buttons and menu for geometry-beta

--- src/components/PillboxContainer.tsx
+++ src/components/PillboxContainer.tsx
@@ -1,40 +1,69 @@
 import { format } from "../i18n/i18n-core";
+import { desModderController } from "../script";
 import "./PillboxContainer.less";
-import { If, Tooltip, For, Switch } from "./desmosComponents";
+import { If, Tooltip, For } from "./desmosComponents";
 import { Component, jsx } from "DCGView";
 import { Calc } from "globals/window";
 import Controller from "main/Controller";
-import { jquery, keys } from "utils/depUtils";
 
 export default class PillboxContainer extends Component<{
   controller: Controller;
+  horizontal: boolean;
 }> {
   controller!: Controller;
+  horizontal!: boolean;
 
   init() {
     this.controller = this.props.controller();
+    this.horizontal = this.props.horizontal();
   }
 
   template() {
     return (
-      <div>
+      <If
+        predicate={() =>
+          this.horizontal === this.controller.showHorizontalPillboxMenu()
+        }
+      >
+        {() => this.templateTrue()}
+      </If>
+    );
+  }
+
+  templateTrue() {
+    return (
+      <div class="dsm-pillbox-and-popover">
         <For each={() => this.controller.pillboxButtonsOrder} key={(id) => id}>
-          <div class="dsm-pillbox-buttons">
+          <div
+            class={{
+              "dsm-pillbox-buttons": true,
+              "dsm-horizontal-pillbox": this.horizontal,
+            }}
+          >
             {(id: string) => (
               <Tooltip
                 tooltip={() =>
                   format(this.controller.pillboxButtons[id].tooltip)
                 }
-                gravity="w"
+                gravity={() => (this.horizontal ? "s" : "w")}
               >
                 <div
-                  class="dcg-btn-flat-gray dcg-settings-pillbox dcg-action-settings dsm-action-menu"
+                  class={() =>
+                    this.horizontal
+                      ? "dcg-icon-btn"
+                      : "dcg-btn-flat-gray dcg-settings-pillbox dcg-action-settings dsm-action-menu"
+                  }
                   role="button"
                   onTap={() => this.onTapMenuButton(id)}
                   // TODO: manageFocus?
-                  style={{
-                    background: Calc.controller.getPillboxBackgroundColor(),
-                  }}
+                  style={() =>
+                    this.horizontal
+                      ? {}
+                      : {
+                          background:
+                            Calc.controller.getPillboxBackgroundColor(),
+                        }
+                  }
                 >
                   <i
                     class={() =>
@@ -47,93 +76,13 @@ export default class PillboxContainer extends Component<{
           </div>
         </For>
         <If predicate={() => this.controller.pillboxMenuOpen !== null}>
-          {() => (
-            <div
-              class="dcg-settings-container dsm-menu-container dcg-left dcg-popover dcg-constrained-height-popover"
-              didMount={() => this.didMountContainer()}
-              didUnmount={() => this.didUnmountContainer()}
-              style={() => ({
-                position: "absolute",
-                ...this.getPopoverPosition(),
-              })}
-            >
-              <Switch key={() => this.controller.pillboxMenuOpen}>
-                {() =>
-                  this.controller.pillboxButtons[
-                    this.controller.pillboxMenuOpen as string
-                  ]?.popup?.(this.controller)
-                }
-              </Switch>
-              <div class="dcg-arrow" />
-            </div>
-          )}
+          {() => desModderController.pillboxMenuView(false)}
         </If>
       </div>
     );
   }
 
-  getPopoverPosition() {
-    let index = this.controller.pillboxButtonsOrder.indexOf(
-      this.controller.pillboxMenuOpen as string
-    );
-    if (Calc.settings.settingsMenu) {
-      index += 1;
-    }
-    // for index=0, this would correspond to the wrench menu,
-    // which is positioned at top=2, right=38.
-    let top = 2;
-    let right = 38;
-    // scale linearly past index=0
-    if (Calc.settings.graphpaper) {
-      top += 43 * index;
-    } else {
-      right += 43 * index;
-    }
-    return {
-      top: top.toString() + "px",
-      right: right.toString() + "px",
-      bottom: 0,
-    };
-  }
-
   onTapMenuButton(id: string) {
     this.controller.toggleMenu(id);
-  }
-
-  didMountContainer() {
-    if (Calc.controller.isGraphSettingsOpen()) {
-      Calc.controller.dispatch({
-        type: "close-graph-settings",
-      });
-    }
-    jquery(document).on(
-      "dcg-tapstart.menu-view wheel.menu-view",
-      (e: Event) => {
-        if (this.eventShouldCloseMenu(e)) {
-          this.controller.closeMenu();
-        }
-      }
-    );
-    jquery(document).on("keydown.menu-view", (e: KeyboardEvent) => {
-      if (keys.lookup(e) === "Esc") {
-        this.controller.closeMenu();
-      }
-    });
-  }
-
-  didUnmountContainer() {
-    jquery(document).off(".menu-view");
-  }
-
-  eventShouldCloseMenu(e: Event) {
-    // this.node refers to the generated node from DCGView
-    const el = jquery(e.target);
-    return (
-      !el.closest(
-        "_domNode" in this._element
-          ? this._element._domNode
-          : this._element._element._domNode
-      ).length && !el.closest(".dsm-action-menu").length
-    );
   }
 }
--- src/main/Controller.ts
+++ src/main/Controller.ts
@@ -1,5 +1,7 @@
+import { DCGView } from "../DCGView";
+import { PillboxContainer } from "../components";
+import PillboxMenu from "../components/PillboxMenu";
 import { List } from "../utils/depUtils";
-import View from "./View";
 import GraphMetadata, {
   Expression as MetadataExpression,
 } from "./metadata/interface";
@@ -34,7 +36,6 @@ interface PillboxButton {
 export default class Controller {
   pluginsEnabled: Map<PluginID, boolean>;
   forceDisabled: Set<string>;
-  view: View | null = null;
   expandedPlugin: string | null = null;
   private expandedCategory: string | null = null;
   pluginSettings: Map<PluginID, GenericSettings>;
@@ -126,7 +127,7 @@ export default class Controller {
     }
   }
 
-  init(view: View) {
+  init() {
     // async
     let numFulfilled = 0;
     listenToMessageDown((message) => {
@@ -142,13 +143,12 @@ export default class Controller {
       // enable once
       numFulfilled += 1;
       if (numFulfilled === 2) {
-        this.view = view;
         for (const { id } of pluginList) {
           if (this.pluginsEnabled.get(id)) {
             this._enablePlugin(id, true);
           }
         }
-        this.view.updateMenuView();
+        this.updateMenuView();
         // cancel listener
         return true;
       }
@@ -170,8 +170,22 @@ export default class Controller {
     }
   }
 
+  pillboxButtonsView(horizontal: boolean) {
+    return DCGView.createElement(PillboxContainer as any, {
+      controller: () => this,
+      horizontal: DCGView.const(horizontal),
+    });
+  }
+
+  pillboxMenuView(horizontal: boolean) {
+    return DCGView.createElement(PillboxMenu as any, {
+      controller: () => this,
+      horizontal: DCGView.const(horizontal),
+    });
+  }
+
   updateMenuView() {
-    this.view?.updateMenuView();
+    Calc.controller.updateViews();
   }
 
   addPillboxButton(info: PillboxButton) {
@@ -190,6 +204,10 @@ export default class Controller {
     this.updateMenuView();
   }
 
+  isSomePillboxMenuOpen() {
+    return this.pillboxMenuOpen !== null;
+  }
+
   toggleMenu(id: string) {
     this.pillboxMenuOpen = this.pillboxMenuOpen === id ? null : id;
     this.pillboxMenuPinned = false;
@@ -205,6 +223,18 @@ export default class Controller {
   toggleMenuPinned() {
     this.pillboxMenuPinned = !this.pillboxMenuPinned;
     this.updateMenuView();
+  }
+
+  showHorizontalPillboxMenu() {
+    // Constant threshold, independent of this.controller.pillboxButtonsOrder.length
+    // Maybe want to tweak the threshold if a fourth possible pillbox button is
+    // added, or figure out a better layout at that point because it's starting
+    // to be a lot of pillbox threshold.
+    return (
+      !Calc.settings.graphpaper ||
+      (Calc.controller.isGeoUIActive() &&
+        Calc.controller.computeMajorLayout().grapher.width > 500)
+    );
   }
 
   getPlugin(id: PluginID) {
