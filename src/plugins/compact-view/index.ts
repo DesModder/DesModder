60071c63cf6db76142063f3002ef377739f1f39f
radian628 <70352880+radian628@users.noreply.github.com>
2023-07-02 21:59:46 -0700

    added hidden evaluations

--- src/plugins/compact-view/index.ts
+++ src/plugins/compact-view/index.ts
@@ -2,6 +2,7 @@ import { PluginController } from "../PluginController";
 import "./compact.css";
 import "./compact.less";
 import { Config, configList } from "./config";
+import { Calc } from "globals/window";
 
 function toggleBodyClass(className: string, bool: boolean) {
   document.body.classList.toggle(className, bool);
@@ -51,11 +52,43 @@ export default class CompactView extends PluginController<Config> {
       "--dsm-compact-mode-multiplier",
       `${this.settings.compactFactor}`
     );
+
+    if (this.settings.hideEvaluations) {
+      this.updateHiddenEvaluations();
+    }
   }
 
-  updateHiddenEvaluations() {}
+  updateHiddenEvaluations() {
+    const mathitems = document.querySelectorAll(
+      ".dcg-mathitem.dcg-expressionitem"
+    );
+
+    for (const item of mathitems) {
+      if (item instanceof HTMLElement) {
+        const rootblock = item.querySelector(".dcg-main .dcg-mq-root-block");
+        const rootBlockWidth = rootblock?.getBoundingClientRect().width ?? 0;
+
+        let rootblockInnerWidth = 0;
+        if (rootblock?.firstChild && rootblock?.lastChild) {
+          const range = new Range();
+          range.setStartBefore(rootblock?.firstChild);
+          range.setEndAfter(rootblock?.lastChild);
+          rootblockInnerWidth = range.getBoundingClientRect().width;
+        }
+
+        const evalMaxWidth = rootBlockWidth - rootblockInnerWidth;
+        const evaluation = item.querySelector(".dcg-evaluation");
+        if (evaluation && evaluation instanceof HTMLElement)
+          evaluation.style.maxWidth = `${Math.max(evalMaxWidth - 5, 20)}px`;
+      }
+    }
+  }
 
   afterEnable() {
+    Calc.controller.dispatcher.register((e) => {
+      if (!this.settings.hideEvaluations) return;
+      this.updateHiddenEvaluations();
+    });
     this.afterConfigChange();
     document.body.classList.add("compact-view-enabled");
   }
