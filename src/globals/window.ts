f1907f92f098f9432106b9dba5e1201a23b741d6
Jared Hughes <jahughes241@gmail.com>
2023-07-12 02:20:21 -0700

    Fix audit-langs script
    It imported from a file that had side effects :(
    
    Fixes #650

--- src/globals/window.ts
+++ src/globals/window.ts
@@ -151,5 +151,3 @@ declare global {
  * Use `/* eslint-disable no-console` and lowercase `console.log` on node scripts
  */
 export const Console = ((globalThis ?? window) as any).console;
-
-export const DesModderFragile = window.DesModderFragile;
