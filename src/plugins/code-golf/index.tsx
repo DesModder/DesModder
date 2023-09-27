829d38394451323ae0cc0fbae7ed90b7ba9cf19d
radian628 <70352880+radian628@users.noreply.github.com>
2023-09-26 19:24:26 -0700

    no golf info for empty exprs

--- src/plugins/code-golf/index.tsx
+++ src/plugins/code-golf/index.tsx
@@ -3,6 +3,7 @@ import { format } from "localization/i18n-core";
 import { Inserter, PluginController } from "../PluginController";
 import "./index.less";
 import { ExpressionModel } from "src/globals";
+import { IfElse } from "src/components";
 
 export class ExpressionItemCostPanel extends Component<{
   model: () => ExpressionModel;
@@ -11,72 +12,77 @@ export class ExpressionItemCostPanel extends Component<{
   rootblock: Element | null | undefined = null;
 
   template() {
-    return (
-      <div class="dsm-code-golf-char-count-container">
-        <div class="dsm-code-golf-char-count">
-          <div>
-            {() =>
-              format("code-golf-char-count", {
-                chars: this.props.model().latex?.length?.toString() ?? "0",
-              })
-            }
-          </div>
-          <div>
-            {() => {
-              const tempRootblock = this.props
-                .model()
-                .dcgView?._element._domNode?.querySelector(
+    const chars = () => this.props.model().latex?.length ?? 0;
+
+    return IfElse(() => chars() > 0, {
+      true: () => (
+        <div class="dsm-code-golf-char-count-container">
+          <div class="dsm-code-golf-char-count">
+            <div>
+              {() =>
+                format("code-golf-char-count", {
+                  chars: chars().toString() ?? "0",
+                })
+              }
+            </div>
+            <div>
+              {() => {
+                const tempRootblock = this.props
+                  .model()
+                  .dcgView?._element._domNode?.querySelector(
+                    ".dcg-main .dcg-mq-root-block"
+                  );
+
+                if (tempRootblock) this.rootblock = tempRootblock;
+
+                if (!this.rootblock) return "0px";
+
+                if (!this.rootblock.lastChild || !this.rootblock.firstChild)
+                  return "0px";
+
+                const range = document.createRange();
+                range.setStartBefore(this.rootblock.firstChild);
+                range.setEndAfter(this.rootblock.lastChild);
+
+                const width = range.getBoundingClientRect().width;
+
+                return format("code-golf-width-in-pixels", {
+                  pixels: Math.round(width).toString(),
+                });
+              }}
+            </div>
+            <div>
+              {() => {
+                const el = this.props.model().dcgView?._element._domNode;
+
+                const tempRootblock = el?.querySelector(
                   ".dcg-main .dcg-mq-root-block"
                 );
+                if (tempRootblock) this.rootblock = tempRootblock;
 
-              if (tempRootblock) this.rootblock = tempRootblock;
+                function symbolCount2(el: Element) {
+                  const svgLen = [".dcg-mq-fraction", "svg", ".dcg-mq-token"]
+                    .map((s) => el.querySelectorAll(s).length)
+                    .reduce((a, b) => a + b);
+                  return (
+                    svgLen +
+                    (el.textContent?.replace(
+                      /\s|[\u00A0\u1680\u2000-\u200B\u202F\u205F\u3000]/g,
+                      ""
+                    )?.length ?? 0)
+                  );
+                }
 
-              if (!this.rootblock) return "0px";
-
-              if (!this.rootblock.lastChild || !this.rootblock.firstChild)
-                return "0px";
-
-              const range = document.createRange();
-              range.setStartBefore(this.rootblock.firstChild);
-              range.setEndAfter(this.rootblock.lastChild);
-
-              const width = range.getBoundingClientRect().width;
-
-              return format("code-golf-width-in-pixels", {
-                pixels: Math.round(width).toString(),
-              });
-            }}
-          </div>
-          <div>
-            {() => {
-              const el = this.props.model().dcgView?._element._domNode;
-
-              const tempRootblock = el?.querySelector(
-                ".dcg-main .dcg-mq-root-block"
-              );
-              if (tempRootblock) this.rootblock = tempRootblock;
-
-              function symbolCount2(el: Element) {
-                const svgLen = [".dcg-mq-fraction", "svg", ".dcg-mq-token"]
-                  .map((s) => el.querySelectorAll(s).length)
-                  .reduce((a, b) => a + b);
-                return (
-                  svgLen +
-                  (el.textContent?.replace(
-                    /\s|[\u00A0\u1680\u2000-\u200B\u202F\u205F\u3000]/g,
-                    ""
-                  )?.length ?? 0)
-                );
-              }
-
-              return format("code-golf-symbol-count", {
-                elements: this.rootblock ? symbolCount2(this.rootblock) : 0,
-              });
-            }}
+                return format("code-golf-symbol-count", {
+                  elements: this.rootblock ? symbolCount2(this.rootblock) : 0,
+                });
+              }}
+            </div>
           </div>
         </div>
-      </div>
-    );
+      ),
+      false: () => <div></div>,
+    });
   }
 }
 
