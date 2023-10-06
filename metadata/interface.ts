61b1d0563bc38382ae3bc9a9db1b9debc18d5e67
radian628 <70352880+radian628@users.noreply.github.com>
2023-10-05 20:54:23 -0700

    removed symbol remappings from metadata

--- metadata/interface.ts
+++ metadata/interface.ts
@@ -1,7 +1,5 @@
 export default interface Metadata {
   version: 2;
-  // Record<GraphHash, Record<OldVarName, NewVarName>>
-  symbolRemappings?: Record<string, Record<string, string>>;
   expressions: Record<string, Expression | undefined>;
 }
 
