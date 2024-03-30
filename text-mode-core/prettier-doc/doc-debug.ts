67bf67ee8631b0b515f3ebf72e578fd76279e95e
Jared Hughes <jahughes241@gmail.com>
2024-03-30 03:33:04 -0700

    Use symbol.description

--- text-mode-core/prettier-doc/doc-debug.ts
+++ text-mode-core/prettier-doc/doc-debug.ts
@@ -198,8 +198,7 @@ export function printDocToDebug(doc: Doc) {
       return printedSymbols[id];
     }
 
-    // TODO: use Symbol.prototype.description instead of slice once Node 10 is dropped
-    const prefix = String(id).slice(7, -1) || "symbol";
+    const prefix = id.description ?? "symbol";
     for (let counter = 0; ; counter++) {
       const key = prefix + (counter > 0 ? ` #${counter}` : "");
       if (!usedKeysForSymbols.has(key)) {
