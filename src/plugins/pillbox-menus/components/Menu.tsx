0ca8d823d2b314daff3fa96e91f25fbbb12460d7
ronwnor <ron123online@gmail.com>
2024-05-12 16:10:31 +0900

    show version number in the plugins menu

--- src/plugins/pillbox-menus/components/Menu.tsx
+++ src/plugins/pillbox-menus/components/Menu.tsx
@@ -74,7 +74,10 @@ export default class Menu extends Component<{
   template() {
     return (
       <div class="dcg-popover-interior">
-        <div class="dcg-popover-title">{format("menu-desmodder-plugins")}</div>
+        <div class="dcg-popover-title">
+          {format("menu-desmodder-plugins")}
+          <div class="dsm-version-number">v{VERSION}</div>
+        </div>
         {categories.map((category) => (
           <div
             class="dcg-options-menu-section dsm-category-section"
