dc32e8e3bafcf8da4dcf21c467cde34200cd363a
Jared Hughes <jahughes241@gmail.com>
2024-07-19 02:40:47 -0700

    Allow tap to still show sticky tooltip

--- src/plugins/hide-errors/components/ErrorTriangle.tsx
+++ src/plugins/hide-errors/components/ErrorTriangle.tsx
@@ -5,7 +5,8 @@ import { jsx } from "#DCGView";
 export function ErrorTriangle(hideErrors: HideErrors, id: string, inner: any) {
   return (
     <div
-      onTap={(event: MouseEvent) =>
+      // Use onClick instead of onTap to allow propagating the event
+      onClick={(event: MouseEvent) =>
         event.shiftKey && hideErrors?.toggleErrorHidden(id)
       }
       class={() => ({
