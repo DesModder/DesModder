5b9f9feb20a5edd177e757d0a0f08dc16ffa4f25
64 <172265251+64qq@users.noreply.github.com>
2024-10-25 20:15:15 +0900

    apply changes suggested by ronwnor (1)
    (DesModder/DesModder/pull/941#discussion_r1816145028)
    prevent any interactions when trying to paste plain text with no expression / focusable element selected

--- src/plugins/paste-image/index.ts
+++ src/plugins/paste-image/index.ts
@@ -15,24 +15,24 @@ export default class PasteImage extends PluginController {
   }
 
   _pasteHandler(e: ClipboardEvent) {
-    if (!this.calc.selectedExpressionId) {
-      // avoid images being inserted at the top of the expressions list
-      // when there is no selected expression
-      if (document.activeElement !== document.body) return;
-      const lastExpression: Desmos.ExpressionState = this.calc
-        .getExpressions()
-        .splice(-1)[0];
-      this.cc.dispatch({
-        type: "set-focus-location",
-        location: {
-          type: "expression",
-          id: lastExpression.id,
-        },
-      });
-    }
     const clipboardFiles = e.clipboardData?.files;
     if (clipboardFiles?.length) {
       e.preventDefault();
+      if (!this.calc.selectedExpressionId) {
+        // avoid images being inserted at the top of the expressions list
+        // when there is no selected expression
+        if (document.activeElement !== document.body) return;
+        const lastExpression: Desmos.ExpressionState = this.calc
+          .getExpressions()
+          .splice(-1)[0];
+        this.cc.dispatch({
+          type: "set-focus-location",
+          location: {
+            type: "expression",
+            id: lastExpression.id,
+          },
+        });
+      }
       this.cc.dispatch({
         type: "new-images",
         files: clipboardFiles,
