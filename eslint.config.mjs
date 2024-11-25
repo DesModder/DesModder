6ec77858c799b4f0a6b8ed98320d95a81c72dbe4
64 <172265251+64qq@users.noreply.github.com>
2024-11-26 00:29:05 +0900

    Migrate configuration to the new flat config
    * Remove .eslintrc.js
    * Create eslint.config.mjs
      * Dedupe ignores for .woff
      * Set ecmaVersion to 2024
    * Use projectService instead of project in parserOptions
      * Remove tsconfig-eslint.json
      * Add tsconfig-build.json to the project graph-state
      * Set option allowJs to true in tsconfig-base.json to fix the errors like "Parsing error: something.js was not found by the project service"
      * Explicitly include the files outside the project to fix TS6307
    * Add typescript.tsdk to .vscode/settings.json
    References:
      * https://typescript-eslint.io/blog/announcing-typescript-eslint-v8/#project-service
      * https://typescript-eslint.io/troubleshooting/typed-linting/#id-like-to-use-tsconfigs-other-than-tsconfigjsons-for-project-service-type-information

---
+++ eslint.config.mjs
@@ -0,0 +1,87 @@
+import rulesDirPlugin from "eslint-plugin-rulesdir";
+import love from "eslint-config-love";
+import eslintConfigPrettier from "eslint-config-prettier";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import tseslint from "typescript-eslint";
+
+rulesDirPlugin.RULES_DIR = "scripts/eslint-rules";
+
+export default tseslint.config(
+  {
+    ignores: [
+      "coverage",
+      "node_modules",
+      "dist",
+      "dist-ts",
+      "hooks",
+      "LICENSE*",
+      // Opt-out instead of opt-in to avoid forgetting to include some js file.
+      "**/*.md",
+      "**/*.json",
+      "**/*.replacements",
+      "**/*.grammar",
+      "**/*.woff",
+      "**/*.css",
+      "**/*.less",
+      "**/*.sh",
+      "**/*.png",
+      "**/*.ftl",
+      "**/*.html",
+      "**/*.svg",
+      "**/*.zip",
+      "eslint.config.mjs",
+    ],
+  },
+  {
+    files: ["**/*.{ts,tsx,js,cjs,mjs}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.es2024,
+      },
+      parser: tsParser,
+      ecmaVersion: 2024,
+      sourceType: "module",
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  // "love" is a new name for "standard-with-typescript"
+  love,
+  eslintConfigPrettier,
+  {
+    rules: {
+      "@typescript-eslint/no-non-null-assertion": "off",
+      "@typescript-eslint/array-type": "off",
+      "@typescript-eslint/strict-boolean-expressions": "off",
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/restrict-template-expressions": "off",
+      "@typescript-eslint/no-confusing-void-expression": [
+        "error",
+        {
+          ignoreArrowShorthand: true,
+        },
+      ],
+      "@typescript-eslint/consistent-type-imports": "off",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "no-console": "error",
+      "@typescript-eslint/no-unsafe-argument": "off",
+    },
+  },
+  {
+    plugins: {
+      rulesdir: rulesDirPlugin,
+    },
+    rules: {
+      "rulesdir/no-format-in-ts": "error",
+      "rulesdir/no-expect-promise": "error",
+      "rulesdir/no-reach-past-exports": "error",
+      "rulesdir/no-external-imports": "error",
+      "rulesdir/no-timeouts-in-intellisense": "error",
+    },
+  }
+);
