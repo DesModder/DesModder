98d096a49c967bd6d743dd57704cb1391bf139a0
64 <172265251+64qq@users.noreply.github.com>
2024-11-26 00:42:01 +0900

    Apply @typescript-eslint/consistent-type-exports
    new feature in typescript-eslint@8.7.0 (export * exports)

--- src/globals/index.ts
+++ src/globals/index.ts
@@ -1,5 +1,5 @@
 import window from "./window";
 export default window;
-export * from "./Calc";
-export * from "./models";
+export type * from "./Calc";
+export type * from "./models";
 export * from "./window";
