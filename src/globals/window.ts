a2863e9209459a5b94c1eef61418ed9072bac447
64 <172265251+64qq@users.noreply.github.com>
2025-05-05 16:23:26 +0900

    Add more detailed types for Desmos.Private
    (cherry picked from commit 5a6c87917f3916fbb071685ed646ceeab69956cf)

--- src/globals/window.ts
+++ src/globals/window.ts
@@ -1,4 +1,5 @@
 import { DCGViewModule } from "../DCGView";
+import Node from "#parsing/parsenode.ts";
 import DSM from "#DSM";
 import {
   CheckboxComponent,
@@ -28,14 +29,19 @@ export interface DWindow extends Window {
     ExpressionView: ExpressionViewComponent;
     ImageIconView: typeof IconViewComponent;
   };
-  Desmos: {
-    Private: {
-      Fragile: typeof Fragile;
-      Mathtools: Mathtools;
-    };
-    MathQuill: {
-      config: (config: MathQuillConfig) => DWindow["Desmos"]["MathQuill"];
-    };
+  Desmos: Desmos;
+}
+
+type DesmosPublic = typeof Desmos;
+interface Desmos extends DesmosPublic {
+  Private: {
+    Fragile: Fragile;
+    Mathtools: Mathtools;
+    Parser: Parser;
+    MathquillConfig: MathquillConfig;
+  };
+  MathQuill: {
+    config: (config: MathQuillConfig) => Desmos["MathQuill"];
   };
 }
 
@@ -54,21 +60,41 @@ interface Mathtools {
   migrateToLatest: (s: GraphState) => GraphState;
 }
 
+export interface Parser {
+  parse: (
+    s: string,
+    config?: {
+      allowDt?: boolean;
+      allowIndex?: boolean;
+      disallowFrac?: boolean;
+      trailingComma?: boolean;
+      seedPrefix?: string;
+      allowIntervalComprehensions?: boolean;
+      disableParentheses?: boolean;
+      disabledFeatures?: string[];
+    }
+  ) => Node;
+}
+
+interface MathquillConfig {
+  getAutoCommands: (options?: {
+    disallowAns?: boolean;
+    disallowFrac?: boolean;
+    additionalCommands?: string[];
+  }) => string;
+  getAutoOperators: (options?: {
+    additionalOperators?: string[];
+    includeGeometryFunctions?: boolean;
+    include3DFunctions?: boolean;
+    newStats?: boolean;
+  }) => string;
+}
+
 declare let window: DWindow;
 
 export default window;
 
-export const Fragile = new Proxy(
-  {},
-  {
-    get(_target, prop) {
-      if ((window as any).Desmos === undefined) return undefined;
-      const fragile = (window as any).Desmos?.Private?.Fragile;
-      if (fragile === undefined) return undefined;
-      return fragile[prop];
-    },
-  }
-) as {
+interface Fragile {
   DCGView: DCGViewModule;
   PromptSliderView: any;
   Checkbox: typeof CheckboxComponent;
@@ -99,21 +125,30 @@ export const Fragile = new Proxy(
     moveItemsTo: (listModel: any, from: number, to: number, n: number) => void;
   };
   currentLanguage: () => string;
-};
+}
+
+export const Fragile = new Proxy(
+  // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+  {} as Fragile,
+  {
+    get(_target, prop: keyof Fragile) {
+      return window.Desmos?.Private?.Fragile?.[prop];
+    },
+  }
+);
 
 type Section = "colors-only" | "lines" | "points" | "fill" | "label" | "drag";
 
+type Private = Desmos["Private"];
 export const Private = new Proxy(
-  {},
+  // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+  {} as Private,
   {
-    get(_target, prop) {
-      if ((window as any).Desmos === undefined) return undefined;
-      const priv = (window as any).Desmos.Private;
-      if (priv === undefined) return undefined;
-      return priv[prop];
+    get(_target, prop: keyof Private) {
+      return window.Desmos?.Private?.[prop];
     },
   }
-) as any;
+);
 
 /* Object.fromEntries based on https://dev.to/svehla/typescript-object-fromentries-389c */
 type DeepWriteable<T> = { -readonly [P in keyof T]: DeepWriteable<T[P]> };
