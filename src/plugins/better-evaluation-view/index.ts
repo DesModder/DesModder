f7e85ff0acf8013223084e3225919fc3bb6d9d9e
64 <172265251+64qq@users.noreply.github.com>
2025-06-24 23:04:38 +0900

    Subscribe to config changes as well

--- src/plugins/better-evaluation-view/index.ts
+++ src/plugins/better-evaluation-view/index.ts
@@ -6,16 +6,16 @@ import { ColorEvaluation } from "./components/ColorEvaluation";
 import { ListEvaluation } from "./components/ListEvaluation";
 import { Config, configList } from "./config";
 
+type EvaluableConstantValue = TypedConstantValue<
+  ConstantListValueType | ValueType.RGBColor
+>;
+
 export default class BetterEvaluationView extends PluginController<Config> {
   static id = "better-evaluation-view" as const;
   static enabledByDefault = true;
   static config = configList;
 
-  evaluation(
-    val: () =>
-      | TypedConstantValue<ConstantListValueType | ValueType.RGBColor>
-      | undefined
-  ): Replacer {
+  evaluation(val: () => EvaluableConstantValue | undefined): Replacer {
     const { settings } = this;
     const value = val();
     if (!value) return undefined;
@@ -49,9 +49,16 @@ export default class BetterEvaluationView extends PluginController<Config> {
     }
   }
 
+  evaluationUpdateKey(container: EvaluationContainerComponent) {
+    const evaluation = this.evaluation(
+      this.getTypedConstantValue.bind(container)
+    );
+    return !!evaluation && this.getTypedConstantValue.call(container);
+  }
+
   getTypedConstantValue(this: EvaluationContainerComponent) {
     const model = this.controller.getItemModel(this.props.id());
     if (model?.type !== "expression") return undefined;
-    return model.formula?.typed_constant_value;
+    return model.formula?.typed_constant_value as EvaluableConstantValue;
   }
 }
