736664fdf48d8884f7f300d5767e97ab2c90069b
Ezra Oppenheimer <111143093+eoppenheimer@users.noreply.github.com>
2025-06-24 21:22:00 -0400

    increasing max if someone has the desire

--- src/plugins/quake-pro/config.ts
+++ src/plugins/quake-pro/config.ts
@@ -4,11 +4,9 @@ export const configList = [
     type: "number",
     default: 1,
     min: 1,
-    max: 10,
+    max: 30,
     step: 0.1,
   },
-  // `as const` ensures that the key values can be used as types
-  // instead of the type 'string'
 ] as const;
 
 export interface Config {
